c3eac36f20c0ccd8babc6ff2fc6eee49c2ef49e6
Raul Viigipuu <raul.viigipuu@smit.ee>
2021-11-23 11:40:26 +0200

    ZonedDateTimeDeserializer

--- src/main/java/dev/jsonone/Item.java
+++ src/main/java/dev/jsonone/Item.java
@@ -2,11 +2,11 @@ package dev.jsonone;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-import java.time.Instant;
+import java.time.ZonedDateTime;
 
 public class Item {
 
     public String title;
-    @JsonDeserialize(using = InstantDeserializer.class)
-    public Instant someDate;
+    @JsonDeserialize(using = ZonedDateTimeDeserializer.class)
+    public ZonedDateTime someDate;
 }
