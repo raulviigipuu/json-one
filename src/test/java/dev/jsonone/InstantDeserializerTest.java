caebea4aa29c0781e08db9635458d9b3ca80b802
Raul Viigipuu <raul@viigipuu.ee>
2021-12-26 17:04:59 +0200

    test files

---
+++ src/test/java/dev/jsonone/InstantDeserializerTest.java
@@ -0,0 +1,13 @@
+package dev.jsonone;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class InstantDeserializerTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    } 
+}
